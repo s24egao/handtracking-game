50b6b0934a3efc2f72b654897015bcd5c7dad084
s24egao <99890186+s24egao@users.noreply.github.com>
2022-07-24 21:53:08 +0800

    update background effects

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// A handtracking game based on MediaPipe Hands
+// A hand tracking game based on MediaPipe Hands
 
 const videoElement = document.getElementsByClassName('input')[0];
 let results
@@ -8,9 +8,9 @@ const hands = new Hands({locateFile: (file) => {
 }})
 hands.setOptions({
 	maxNumHands: 2,
-	modelComplexity: 1,
-	minDetectionConfidence: 0.3,
-	minTrackingConfidence: 0.3
+	modelComplexity: 0,
+	minDetectionConfidence: 0.5,
+	minTrackingConfidence: 0.5
 })
 hands.onResults(r => results = r)
 
@@ -40,18 +40,21 @@ const canvas = p => {
 	let combo = 0
 	let maxCombo = 0
 	let updateCombo = 0
+	let lineEffect = 0
+	let noiseAmount = 0
 
 	p.setup = () => {
 		p.createCanvas(p.windowWidth, p.windowHeight)
 			p.textAlign(p.CENTER, p.CENTER)
 		p.strokeWeight(60)
-		p.strokeJoin(p.BEVEL)
+		p.strokeJoin(p.ROUND)
 		p.fill(200)
 		p.background(0)
 		p.ellipseMode(p.CENTER)
 		p.rectMode(p.CENTER)
 		p.pixelDensity(1)
 		for(let i = 0; i < 10; i++) particles.push(new Background())
+		for(let i = 0; i < 10; i++) particles.push(new Bubble())
 	}
 
 	p.draw = () => {
@@ -59,23 +62,22 @@ const canvas = p => {
 		p.noFill()
 		p.stroke(50)
 		p.strokeWeight(1)
-		for(let i = 10; i < p.width; i += 50) {
-			p.line(i, 0, i, p.height)
-		}
-		for(let i = 10 - (p.frameCount / 2) % 50; i < p.height; i += 50) {
-			p.line(0, i, p.width, i)
-		}
 
-		p.stroke(255)
-		p.strokeWeight(2)
+		for(let i = 10; i < p.width; i += 50) p.line(i, 0, i, p.height)
+		for(let i = 10 - (p.frameCount / 2) % 50; i < p.height; i += 50) p.line(0, i, p.width, i)
+
+		p.stroke(255, 200)
+		p.strokeWeight(2 + lineEffect)
+		lineEffect *= 0.9
 		p.circle(p.width / 2, p.height / 2, 750)
 
 		p.strokeWeight(20)
 		p.stroke(50 + updateCombo)
 		p.fill(50 + updateCombo)
 		updateCombo *= 0.9
+		noiseAmount *= 0.9
 		p.textSize(300)
-		p.text(combo, p.width / 2, p.height / 2 - 20)
+		p.text(combo, p.width / 2 + (p.sin(p.frameCount / 2)) * noiseAmount, p.height / 2 - 20)
 		p.strokeWeight(3)
 		p.stroke(50)
 		p.fill(50)
@@ -85,29 +87,30 @@ const canvas = p => {
 
 		points.length = 0
 		if(!results) return
+
 		if(results.multiHandLandmarks) {
-	    for(const [i, landmarks] of results.multiHandLandmarks.entries()) {
-			drawHand(landmarks)
-			points[i] = center(landmarks)
-			points[i].r = (p.atan2(points[i].y - 0.5, -points[i].x + 0.5) + p.TWO_PI) % p.TWO_PI
-			p.noFill()
-			p.strokeWeight(12)
-			p.stroke(200, 50)
-			p.circle(mapX(points[i].x), mapY(points[i].y), 260)
-			p.push()
-			p.strokeCap(p.SQUARE)
-			p.translate(p.width / 2, p.height / 2)
-			p.rotate(points[i].r)
-			p.noFill()
-			p.strokeWeight(6)
-			p.stroke(200, 50)
-			p.arc(0, 0, 700, 700, -0.5, 0.5)
-			p.strokeWeight(20)
-			p.stroke(255)
-			p.arc(0, 0, 700, 700, -0.2, 0.2)
-			p.pop()
-	    }
-	  }
+		    for(const [i, landmarks] of results.multiHandLandmarks.entries()) {
+				drawHand(landmarks)
+				points[i] = center(landmarks)
+				points[i].r = (p.atan2(points[i].y - 0.5, -points[i].x + 0.5) + p.TWO_PI) % p.TWO_PI
+				p.noFill()
+				p.strokeWeight(12)
+				p.stroke(200, 50)
+				p.circle(mapX(points[i].x), mapY(points[i].y), 260)
+				p.push()
+				p.strokeCap(p.SQUARE)
+				p.translate(p.width / 2, p.height / 2)
+				p.rotate(points[i].r)
+				p.noFill()
+				p.strokeWeight(6)
+				p.stroke(200, 50)
+				p.arc(0, 0, 700, 700, -0.5, 0.5)
+				p.strokeWeight(20)
+				p.stroke(255)
+				p.arc(0, 0, 700, 700, -0.2, 0.2)
+				p.pop()
+		    }
+		}
 
 		if(p.frameCount % 30 == 0) particles.push(new Key())
 		if(p.frameCount % 120 == 0) particles.push(new Key())
@@ -198,6 +201,7 @@ const canvas = p => {
 					combo++
 					if(combo > maxCombo) maxCombo = combo
 					updateCombo = 100
+					lineEffect = 10
 					particles.push(new FX2(p.cos(this.r) * 400, p.sin(this.r) * 400))
 					for(let i = 0; i < 10; i++) particles.push(new FX(p.cos(this.r) * 400, p.sin(this.r) * 400))
 					this.active = false
@@ -208,6 +212,7 @@ const canvas = p => {
 			if(this.time <= 0) {
 				combo = 0
 				particles.push(new Miss(p.cos(this.r) * 400, p.sin(this.r) * 400))
+				noiseAmount = 30
 				this.active = false
 			}
 		}
@@ -220,19 +225,20 @@ const canvas = p => {
 			this.r = p.random(p.TWO_PI)
 			this.vx = p.cos(this.r) * 15
 			this.vy = p.sin(this.r) * 15
-			this.time = 60 + p.random(0, 60)
+			this.s = p.random(5, 20)
+			this.time = 60
 			this.active = true
 		}
 
 		update() {
 			this.time--
-			this.vx *= 0.9
-			this.vy = this.vy * 0.99 + 1
+			this.vx *= 0.95
+			this.vy = this.vy * 0.95 + 1
 			this.x += this.vx
 			this.y += this.vy
 			p.noStroke()
-			p.fill(255, 90)
-			p.circle(this.x, this.y, 10)
+			p.fill(255, this.time * 4)
+			p.circle(this.x, this.y, this.s)
 			if(this.time <= 0) this.active = false
 		}
 	}
@@ -290,6 +296,42 @@ const canvas = p => {
 			p.stroke(200, 20)
 			p.noFill()
 			p.arc(0, 0, this.s, this.s, 0, this.r)
+			p.pop()
+		}
+	}
+
+	class Bubble {
+		constructor() {
+			this.x = p.random(0, p.width)
+			this.y = p.random(0, p.height)
+			this.n = p.random(0, 100)
+			this.vx = 0
+			this.vy = 0
+			this.s = p.random(100, 800)
+			this.active = true
+		}
+
+		update() {
+			for(let point of points) {
+				if(p.dist(mapX(point.x), mapY(point.y), this.x, this.y) < 300) {
+					this.vx += (this.x - mapX(point.x)) / 100
+					this.vy += (this.y - mapY(point.y)) / 100
+				}
+			}
+			this.vx = this.vx * 0.95 + (p.noise(this.n, p.frameCount / 100) - 0.5) * 0.5
+			this.vy = this.vy * 0.95 + (p.noise(this.n + 100, p.frameCount / 100) - 0.5) * 0.5
+			this.x += this.vx
+			this.y += this.vy
+			this.x = (this.x + p.width) % p.width
+			this.y = (this.y + p.height) % p.height
+			p.push()
+			p.noStroke()
+			p.fill(50, 50)
+			p.circle(this.x, this.y, this.s)
+			if(this.x > p.width - this.s / 2) p.circle(this.x - p.width, this.y, this.s)
+			if(this.y > p.height - this.s / 2) p.circle(this.x, this.y - p.height, this.s)
+			if(this.x < this.s / 2) p.circle(this.x + p.width, this.y, this.s)
+			if(this.y < this.s / 2) p.circle(this.x, this.y + p.height, this.s)
 			p.pop()
 		}
 	}
